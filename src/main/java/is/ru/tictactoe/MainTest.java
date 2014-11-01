e5f67305df313a7dcc2e53549988f716a95df515
Kristrun <kristrun12@ru.is>
2014-11-01 18:33:20 +0000

    Minimal main function added to be able to run code.

--- src/main/java/is/ru/tictactoe/MainTest.java
+++ src/main/java/is/ru/tictactoe/MainTest.java
@@ -1,15 +1,26 @@
 package is.ru.tictactoe;
 
-public class MainTest{
+public class MainTest
+{
 	public static void main (String[] args) throws Exception
 	{
 	
-		/*Board b = new Board();
-		int numbPlayers = b.mode();
-		 
-		 char board2 [][];
-		 board2 = b.clearBoard(board2);
-		 game.PrintTheBoard(board2);*/
+		Board b = new Board();
+		int numbPlayers = 1;
+		boolean won = false;
 	
+		char board[][] = b.clearBoard();
+		
+		while(won != true)
+		{
+			int pmNumb = Board.placeMarker(1,1,1);
+			pmNumb = Board.placeMarker(2,1,2);
+			pmNumb = Board.placeMarker(1, 2, 1);
+			pmNumb = Board.placeMarker(2, 2, 2);
+			pmNumb = Board.placeMarker(1, 3, 2);
+			won = true;
+		}
+		System.out.println("Player one won");
 	}
+	
 }
